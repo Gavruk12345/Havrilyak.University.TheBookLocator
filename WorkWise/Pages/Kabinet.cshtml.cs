5abdfc7a480d68c8090ebb1190370fea9d39c7fb
Gavruk12345 <game.master.kvx@gmail.com>
2023-04-06 09:30:59 +0300

    Add cabinet

---
+++ WorkWise/Pages/Kabinet.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WorkWise.Pages
+{
+    public class KabinetModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

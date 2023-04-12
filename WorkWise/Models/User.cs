0e61492b4c62ac0478fb86744b5ce9bf35dc65e2
Gavruk12345 <game.master.kvx@gmail.com>
2023-04-12 15:18:23 +0300

    Update Cabinet Page

---
+++ WorkWise/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WorkWise.Controllers;
+
+namespace WorkWise.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+
+        public string? FirstName { get; set; }
+
+
+        public string? LastName { get; set; }
+
+
+        public string? Email { get; set; }
+
+
+        public string? Phone { get; set; }
+
+
+        public string? Password { get; set; }
+    }
+
+}

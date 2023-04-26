c24f8b96ced3870912c118c87cf391608db1bc4b
Gavruk12345 <game.master.kvx@gmail.com>
2023-04-27 01:19:01 +0300

    Freelancer page

---
+++ WorkWise/Pages/Freelancer/programing.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WorkWise.Pages.Freelancer
+{
+    public class programingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

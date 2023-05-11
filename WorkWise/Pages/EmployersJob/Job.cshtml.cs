f88cfaaee55536ab5e51421fafe00cdd34c61078
Gavruk12345 <game.master.kvx@gmail.com>
2023-05-11 09:54:46 +0300

    Add job page , add profile freelancer V1,  add job profile

---
+++ WorkWise/Pages/EmployersJob/Job.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WorkWise.Pages.EmployersJob
+{
+    public class JobModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WorkWise/Pages/FrelancerProfile/FreelancerPro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WorkWise.Pages.FrelancerProfile
+{
+    public class FreelancerProModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

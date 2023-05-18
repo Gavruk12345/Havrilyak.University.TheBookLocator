17718597057c56df4d73ae50bb268662a466dfc4
Gavruk12345 <game.master.kvx@gmail.com>
2023-05-18 11:50:10 +0300

    update

---
+++ WorkWise/Pages/Employers/CreateJob.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WorkWise.Pages.Employers
+{
+    public class CreateJobModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
